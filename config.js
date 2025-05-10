2dd66d75bd21503a33527c012309a7ee48b3e373
tienanh <truongtienanh16@gmail.com>
2025-05-10 09:30:55 +0700

    update

--- config.js
+++ config.js
@@ -1,10 +1,3 @@
-// config.js
-// const ENV = {
-//   EMAILJS_PUBLIC_KEY: 'delMucxculOatAaX2',
-//   EMAILJS_SERVICE_ID: 'service_zgt0qng',
-//   EMAILJS_TEMPLATE_ID_1: 'template_3vgmpvh',
-//   EMAILJS_TEMPLATE_ID_2: 'template_vle3l5v'
-// };
 window.ENV = {
   EMAILJS_PUBLIC_KEY: 'delMucxculOatAaX2',
   EMAILJS_SERVICE_ID: 'service_zgt0qng',
