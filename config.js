20fa90888632adf4013649e2a0cf55586fc779ee
tienanh <truongtienanh16@gmail.com>
2025-05-10 09:27:07 +0700

    update

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js
+// const ENV = {
+//   EMAILJS_PUBLIC_KEY: 'delMucxculOatAaX2',
+//   EMAILJS_SERVICE_ID: 'service_zgt0qng',
+//   EMAILJS_TEMPLATE_ID_1: 'template_3vgmpvh',
+//   EMAILJS_TEMPLATE_ID_2: 'template_vle3l5v'
+// };
+window.ENV = {
+  EMAILJS_PUBLIC_KEY: 'delMucxculOatAaX2',
+  EMAILJS_SERVICE_ID: 'service_zgt0qng',
+  EMAILJS_TEMPLATE_ID_1: 'template_3vgmpvh',
+  EMAILJS_TEMPLATE_ID_2: 'template_vle3l5v',
+};
